0c97280b642f11e7ac66cd920864d83dabc47d42
alpha4637 <tyagigaurav345@gmail.com>
2024-06-14 12:37:49 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [allTodos, setTodos]= useState([]);
   const [newTitle, setNewTitle] = useState("");
   const[newDescription, setNewDescription] = useState("");
+  const[completedTodos, setCompletedTodos]= useState([]);
 
   // Function to handle adding a new todo item
   const handleAddTodo = ()=>{
@@ -32,14 +33,48 @@ function App() {
     let reducedTodo = [...allTodos];
     reducedTodo.splice(index);
     localStorage.setItem('todolist', JSON.stringify(reducedTodo));
-    setTodos(reducedTodo)
+    setTodos(reducedTodo)   
   }
 
+  const handleComplete =(index)=>{
+    let now = new Date();
+    let dd =now.getDate();
+    let mm = now.getMonth();
+    let yyyy = now.getFullYear();
+    let h= now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+    let completedOn = dd + '-' + mm + '-' + yyyy + ' at ' + h + ':' + m + ':' + s;
+
+    let filteredItem= {
+      ...allTodos[index],
+      completedOn: completedOn
+    }
+
+    let updatedCompletedArr = [...completedTodos];
+    updatedCompletedArr.push(filteredItem);
+    setCompletedTodos(updatedCompletedArr);
+    handleDeleteTodo(index);   
+
+    localStorage.setItem('completedTodos', JSON.stringify(updatedCompletedArr));
+  }
+
+  const handleDeleteCompletedTodo = (index)=>{
+    let reducedTodo = [...completedTodos];
+    reducedTodo.splice(index);
+    localStorage.setItem('completedTodos', JSON.stringify(reducedTodo));
+    setCompletedTodos(reducedTodo) 
+  }
   //useEffect to load saved todo items from the local storage when the component mounts
   useEffect(()=>{
     let savedTodo = JSON.parse(localStorage.getItem('todolist'));
+    let savedCompletedTodo = JSON.parse(localStorage.getItem('completedTodos'));
     if(savedTodo){
       setTodos(savedTodo);
+    }
+
+    if (savedCompletedTodo) {
+      setCompletedTodos(savedCompletedTodo);
     }
   },[])
   return (    
@@ -60,27 +95,39 @@ function App() {
           </div>
         </div>
         <div className='todo-btn-wrapper'>
-          <button className={`secondaryBtn ${isCompleted=== false && 'active'}`} onClick={()=> setIsCompleted(false)}>Todo</button>
-          <button className={`secondaryBtn ${isCompleted=== true && 'active'}`} onClick={()=> setIsCompleted(true)}>Completed</button>
+          <button className={`secondaryBtn ${isCompleted === false && 'active'}`} onClick={()=> setIsCompleted(false)}>Todo</button>
+          <button className={`secondaryBtn ${isCompleted === true && 'active'}`} onClick={()=> setIsCompleted(true)}>Completed</button>
         </div>
-        <div className='todo-list-wrapper'>
-          {/* <div className='todo-list-item'>
-            <h3>Task 1</h3>
-            <p>Description</p>
-            <div className='icon-div'>          
-            <RiDeleteBin6Line />
-            <MdDone />
-            </div>
-          </div> */}
-          {allTodos.map((item,index)=>{
+        <div className='todo-list-wrapper'>        
+          {isCompleted === false && allTodos.map((item,index)=>{
             return(
               <div className='todo-list-item' key={index}>
+                <div>
             <h3>{item.title}</h3>
             <p>{item.description}</p>
-
+            </div>
             <div className='icon-div'>          
             <RiDeleteBin6Line onClick={()=>handleDeleteTodo(index)} title='delete'/>
-            <MdDone />
+            <MdDone onClick={()=> handleComplete(index)}/>
+            </div>
+          </div> 
+            )
+          })}
+           {isCompleted === true && completedTodos.map((item,index)=>{
+            return(
+              <div className='todo-list-item' key={index}>
+                <div>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p>
+              <small>
+                Completed on: {item.completedOn}
+              </small>
+            </p>
+            </div>
+            <div className='icon-div'>          
+            <RiDeleteBin6Line onClick={()=>handleDeleteCompletedTodo(index)} title='delete'/>
+            {/* <MdDone onClick={()=> handleComplete(index)}/> */}
             </div>
           </div> 
             )
